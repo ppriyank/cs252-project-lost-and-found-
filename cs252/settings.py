1720855119cd78c607b6558bd6bfdcf82fd9e93b
ppriyank <priyankpathak50@gmail.com>
2016-10-18 12:25:20 +0000

    configured for mysql

--- cs252/settings.py
+++ cs252/settings.py
@@ -73,12 +73,24 @@ WSGI_APPLICATION = 'cs252.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'lost',
+        'USER': 'root',
+        'PASSWORD': 'yo',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
